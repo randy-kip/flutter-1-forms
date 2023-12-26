c025091f5f5bdcfcdb3dcfa010a894fa2a702fbc
Randy Kipkurui <randy.kipkurui4@gmail.com>
2023-12-26 15:26:12 +0300

    feat: Create text input file for a basic text input widget.

---
+++ lib/widgets/text_input.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+class TextInput extends StatefulWidget {
+  const TextInput({super.key});
+
+  @override
+  State<StatefulWidget> createState() => _TextInputState();
+}
+
+class _TextInputState extends State<TextInput> {
+  final textController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+
+    textController.addListener(() {
+      if (kDebugMode) {
+        print("Current value is ${textController.text}");
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+
+    textController.dispose();
+    }
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      keyboardType: TextInputType.text,
+      decoration: const InputDecoration(labelText: "Name"),
+      maxLength: 50,
+      controller: textController,
+    );
+  }
+}
